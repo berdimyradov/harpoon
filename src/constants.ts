b4ec207c80db42da4156ecf9febe88214d088703
rovshen99 <80780914+rovshen99@users.noreply.github.com>
2023-04-23 15:39:52 +0400

    pikachu I choose you

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,7 @@ export const pokemon_order = [
 
 export const POKEMONS_IS_CAUGHT_BY: Record<number, string> = {
   46: "berdimyradov",
+  85: "rovshen99",
 } as const;
 
 export const POKEMONS_IS_SHINY: Record<number, boolean> = {
