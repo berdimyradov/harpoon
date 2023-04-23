2d820c1889848e40fda15e855ce6de33d3b78d8c
berdimyradov <kberdimyradov@gmail.com>
2023-04-23 14:28:50 +0300

    :bug: Removed mocked data

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,6 @@ export const pokemon_order = [
 
 export const POKEMONS_IS_CAUGHT_BY: Record<number, string> = {
   46: "berdimyradov",
-  56: "rovshen99",
 } as const;
 
 export const POKEMONS_IS_SHINY: Record<number, boolean> = {
